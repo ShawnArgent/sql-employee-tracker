78999930961e3ab42a5bb8839a67612630723736
Shawn Argent <kshawn001@gmail.com>
2022-05-01 22:04:11 -0700

    change

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,27 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
+
+
+CREATE TABLE department (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name  VARCHAR (30) NOT NULL
+    );
+
+CREATE TABLE roles (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    title  VARCHAR(30) NOT NULL,
+    salary DECIMAL (10,2) NOT NULL,
+    department_id INT NOT NULL, 
+    FOREIGN KEY(department_id) references department(id)
+	);  
+
 CREATE TABLE Employee (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
     first_name  VARCHAR(30) NOT NULL,
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    foreign key(role_id) references roles(id),
-    foreign key(manager_id) references Employee(manager_id)
+    FOREIGN KEY(role_id) REFERENCES roles(id),
+    FOREIGN KEY(manager_id) REFERENCES Employee(manager_id)
 	); 
-
-CREATE TABLE department (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name  VARCHAR (30) NOT NULL);
-
-CREATE TABLE roles (
-	id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title  VARCHAR(30) NOT NULL,
-    salary DECIMA (10,2) NOT NULL,
-    department_id integer NOT NULL, 
-    foreign key(department_id) references Department(id)
-	);  
-
