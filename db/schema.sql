453c86811d190d8c38ca5eb4a9c1bc0cea647650
Shawn Argent <kshawn001@gmail.com>
2022-05-02 11:08:09 -0700

    changes

--- db/schema.sql
+++ db/schema.sql
@@ -6,24 +6,24 @@ USE employees_db;
 
 
 CREATE TABLE department (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+	id INT PRIMARY KEY,
     name  VARCHAR (30) NOT NULL
     );
 
-CREATE TABLE roles (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE role (
+	id INT PRIMARY KEY,
     title  VARCHAR(30) NOT NULL,
     salary DECIMAL (10,2) NOT NULL,
     department_id INT NOT NULL, 
     FOREIGN KEY(department_id) references department(id)
 	);  
 
-CREATE TABLE Employee (
+CREATE TABLE employee (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     first_name  VARCHAR(30) NOT NULL,
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY(role_id) REFERENCES roles(id),
-    FOREIGN KEY(manager_id) REFERENCES Employee(manager_id)
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
 	); 
--- db/seeds.sql
+++ db/seeds.sql
@@ -6,25 +6,25 @@ VALUES ("Human Resources"),
 
 SELECT * FROM department;
 
-INSERT INTO roles (title, salary, department_id) 
-VALUES  ("HR Director", 100000, 1),
-        ("Benefits Administrator", 85000, 1),
-        ("Chief Financial Officer", 250000, 2),
+INSERT INTO role (title, salary, department_id) 
+VALUES  ("HR Director", 100000),
+        ("Benefits Administrator", 85000),
+        ("Chief Financial Officer", 250000),
         ("Accountant", 95000, 2),
-        ("Marketing Director", 100000, 3),
-        ("Sales Manager", 125000, 4),
-        ("Sales Representative", 85000, 4);
+        ("Marketing Director", 100000),
+        ("Sales Manager", 125000),
+        ("Sales Representative", 85000);
 
         SELECT * FROM Role;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ("Jacob", "Samuel")
-        ("Amber", "Thompson")
-        ("Robert", "Chase")
-        ("Patrick", "O'Brien")
-        ("Kerrie", "Benz")
-        ("Luke", "Anderson")
-        ("Toby", "Arnold")
+VALUES  ("Jacob", "Samuel"),
+        ("Amber", "Thompson"),
+        ("Robert", "Chase"),
+        ("Patrick", "O'Brien"),
+        ("Kerrie", "Benz"),
+        ("Luke", "Anderson"),
+        ("Toby", "Arnold");
 
         SELECT * FROM employee;
  
