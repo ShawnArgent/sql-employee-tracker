ca314c5855ed885279aecf9a32782e762cc72bc8
Shawn Argent <kshawn001@gmail.com>
2022-05-03 10:03:07 -0700

    change port fix

--- emp.js
+++ emp.js
@@ -1,26 +1,21 @@
-const express = require('express');
-
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
 // Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const connection = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
+      port: 3306,
       password: 'password',
       database: 'employees_db'
     });
   
-    connection.connect(function(err){
-      if (err) throw err;
+    connection.connect(function(err) {
+      if (err) throw err
+      console.log("Connected as Id" + connection.threadId)
       start()
     });
 
@@ -126,9 +121,4 @@ const connection = mysql.createConnection(
     });
 }
 
-
-
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-      });
       
